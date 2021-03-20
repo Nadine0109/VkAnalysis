c03d4298f3dcdb5046c9e7972c2e2c5cc197e3be
Nadine0109 <nadine0109@ya.ru>
2021-03-20 12:24:09 +0500

    Edited the code

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,20 +4,20 @@ import ru.netology.domain.Post;
 
 
 public class PostManager {
-    private Post [] posts;
+    private Post[] posts;
 
-    public Post [] getPostsForFeed () {
+    public Post[] getPostsForFeed() {
         //TODO: add logic
         return null;
     }
 
-    public Post [] search (String query, int postID, int ownerId, boolean ownersOnly, String domain) {
+    public Post[] search(String query, int postID, int ownerId, boolean ownersOnly, String domain) {
 
         //TODO: add logic
         return null;
     }
 
-    public void delete (int postId, int ownerId, boolean canDelete) {
+    public void delete(int postId, int ownerId, boolean canDelete) {
         //TODO: add logic
     }
 }
